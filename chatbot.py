a76d5b1a88d78f981d5a2d2d701a1f8f8d1e7d00
Max Leibowitz <403089@bsd48.org>
2020-05-12 09:48:33 -0700

    First commit!

---
+++ chatbot.py
@@ -0,0 +1,103 @@
+# import cobe
+from cobe import brain
+import string
+import re
+#
+# with open("spaceballs.txt") as f:
+#     lines = f.read().splitlines()
+#
+# with open("names.txt") as f:
+#     names = f.read().splitlines()
+#
+# patterns = []
+# for i in names:
+#     patterns.append(re.compile("^" + i))
+#
+#
+# def check(text):
+#     for n in patterns:
+#         if n.match(text):
+#             return True
+#     return False
+#     # print()
+#     # l = 0
+#     # for j in text:
+#     #     if j == j.upper():
+#     #         l += 1
+#     #     else:
+#     #         return 8 > l > 0
+#
+#
+# def remove_names(text):
+#     for p in patterns:
+#         if p.match(text):
+#             return p.sub("", text)
+#
+#
+# # for i in lines[:20]:
+# #     print(i, check(i))
+#     # check(i)
+# filtered = list(filter(check, lines))
+# filtered = list(map(remove_names, filtered))
+# # print(filtered[:10])
+# with open("spaceballs2.txt", "wt") as f:
+#     f.write("\n".join(filtered))
+#
+# exit()
+
+# with open("TOY STORY.txt") as f:
+#     lines = f.read().splitlines()
+#
+# all_lines = []
+# current = ""
+#
+# for i in lines:
+#     if len(i) < len("            "):
+#         continue
+#     if i[:12] == " " * 12 and not i[:25] == " " * 25:
+#         current += i[11:]
+#     if i[:25] == " " * 25:
+#         all_lines.append(current)
+#         current = ""
+#
+# # print(all_lines[:10])
+# with open("toy_story_lines.txt", "wt") as f:
+#     f.write("\n".join(all_lines))
+# exit()
+
+#
+# # p = re.compile()
+# p = re.compile("\\+\\+\\+\\$\\+\\+\\+")
+# # p = re.compile("[\\w|\\s|\\!|\\?|\\@|\\#|\\%|\\^|\\&|\\*|\\(|\\)|\\-|_|\\=|]+$")
+# with open("movie_lines2.txt") as f:
+#     # print(f.read(1000))
+#     lines = list(map(lambda x: p.split(x)[-1][1:], f.read().splitlines()))
+#
+# # print(lines[0:10])
+#
+# with open("movie_lines3.txt", "wt") as f:
+#     f.write("\n".join(lines))
+
+brain = brain.Brain("cobe.brain")
+
+# for i in lines:
+#     brain.learn(i)
+
+# for i in open("messages.txt").read().splitlines():
+#     brain.learn(i)
+
+# brain.learn
+# brain.learn(open("messages.txt").read())
+# print(brain.reply("Hello, I'm max"))
+
+
+def learn_and_reply(message):
+    brain.learn(message)
+    return brain.reply(message)
+
+
+# for i in range(1000):
+#     for j in ["windows good", "microsoft is good", "windows is the best os", "windows is better than linux",
+#               "I hate linux", "linux sucks"]:
+#         brain.learn(j)
+#         brain.learn("no, linux is better than windoze")
---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+import pickle as pkl
+from pprint import pprint
+import chatbot
+from chatbot import learn_and_reply
+import json
+
+secrets = json.loads(open("secrets.json").read())
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or message.channel == "unbelievaboat":
+        # print(message.content)
+        # chatbot.brain.learn(message.content)
+        return
+    # print(message.content)
+    if secrets["chatbot"]["id"] in message.content:
+        # print(str(message.channel))
+        # if str(message.channel) == "bot":
+            # messages = list(map(lambda x: x.content + "\n", await message.channel.history(limit=200).flatten()))
+            # pprint(messages)
+            # messages.reverse()
+            # messages = "\n".join(messages)
+            # with open("messages.txt", "wt") as f:
+            #     f.writelines(messages)
+            # return
+            # with open("channel_messages.txt", "a+", encoding="utf-8") as f:
+            #     print(*messages, sep="\n\n", file=f)
+            # # print(message.content)
+            # return
+            # client.logs_from(message.channel, limit=200)
+            # return
+            # messages = await message.channel.history(limit=200).flatten()
+            # pprint(messages)
+            # pkl.Pickler(open("messages", "wb")).dump(messages)
+        # return
+
+        if str(message.channel) == "bot":
+            # print(message.content[22:])
+            await message.channel.send(chatbot.brain.reply(message.content[22:]).replace("@", "!!").replace("<u>", "_").replace("</u>", "_"))
+    else:
+        chatbot.brain.learn(message.content)
+
+client.run(secrets["chatbot"]["token"])
