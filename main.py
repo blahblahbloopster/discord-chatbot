a547ff3aeab1fcc292762a346389bce10df6efde
Max Leibowitz <403089@bsd48.org>
2020-05-13 15:17:04 -0700

    Added stop and start condition

--- main.py
+++ main.py
@@ -23,29 +23,33 @@ async def on_message(message):
         return
     if secrets["chatbot"]["id"] in message.content or message.content[:4] == ":~$ ":
         content = message.content.replace(secrets["chatbot"]["id"], "").replace(":~$ ", "")
+        if "$$$$" in content:
+            prefix = "<$$$709896631855874088> "
+        if "$%$%" in content:
+            prefix = ""
         # print(str(message.channel))
         # if str(message.channel) == "bot":
-            # messages = list(map(lambda x: x.content + "\n", await message.channel.history(limit=200).flatten()))
-            # pprint(messages)
-            # messages.reverse()
-            # messages = "\n".join(messages)
-            # with open("messages.txt", "wt") as f:
-            #     f.writelines(messages)
-            # return
-            # with open("channel_messages.txt", "a+", encoding="utf-8") as f:
-            #     print(*messages, sep="\n\n", file=f)
-            # # print(message.content)
-            # return
-            # client.logs_from(message.channel, limit=200)
-            # return
-            # messages = await message.channel.history(limit=200).flatten()
-            # pprint(messages)
-            # pkl.Pickler(open("messages", "wb")).dump(messages)
+        # messages = list(map(lambda x: x.content + "\n", await message.channel.history(limit=200).flatten()))
+        # pprint(messages)
+        # messages.reverse()
+        # messages = "\n".join(messages)
+        # with open("messages.txt", "wt") as f:
+        #     f.writelines(messages)
+        # return
+        # with open("channel_messages.txt", "a+", encoding="utf-8") as f:
+        #     print(*messages, sep="\n\n", file=f)
+        # # print(message.content)
+        # return
+        # client.logs_from(message.channel, limit=200)
+        # return
+        # messages = await message.channel.history(limit=200).flatten()
+        # pprint(messages)
+        # pkl.Pickler(open("messages", "wb")).dump(messages)
         # return
 
         if str(message.channel) == "bot":
             # print(message.content[22:])
-            await message.channel.send(("<$$$709896631855874088> " + chatbot.brain.reply(content).replace("@", "!!").replace("<u>", "_").replace("</u>", "_")).replace("$$$", "@"))
+            await message.channel.send((prefix + chatbot.brain.reply(content).replace("@", "!!").replace("<u>", "_").replace("</u>", "_")).replace("$$$", "@"))
             # await message.channel.send("(I'm in observation mode for the moment)")
             time.sleep(1)
     else:
