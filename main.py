1f326ecf3c236b9cbe6fcf3289bdc7a62d509b39
Max Leibowitz <403089@bsd48.org>
2020-05-13 15:36:50 -0700

    fixed

--- main.py
+++ main.py
@@ -17,10 +17,18 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
+prefix = ""
+
+
 @client.event
 async def on_message(message):
+    global prefix
     if message.author == client.user or message.channel == "unbelievaboat" or message.channel == "admin-corner-uwu":
         return
+    if "$$$$" in message.content:
+        prefix = "<$$$709896631855874088> "
+    if "$%$%" in message.content:
+        prefix = ""
     if secrets["chatbot"]["id"] in message.content or message.content[:4] == ":~$ ":
         content = message.content.replace(secrets["chatbot"]["id"], "").replace(":~$ ", "")
         if "$$$$" in content:
@@ -49,7 +57,7 @@ async def on_message(message):
 
         if str(message.channel) == "bot":
             # print(message.content[22:])
-            await message.channel.send((prefix + chatbot.brain.reply(content).replace("@", "!!").replace("<u>", "_").replace("</u>", "_")).replace("$$$", "@"))
+            await message.channel.send((prefix + chatbot.brain.reply(content).replace("@", "!!").replace("<u>", "_").replace("</u>", "_")).replace("$$$", "@"), tts=True)
             # await message.channel.send("(I'm in observation mode for the moment)")
             time.sleep(1)
     else:
