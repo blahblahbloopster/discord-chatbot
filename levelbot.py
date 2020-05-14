d43e3ad7c150e7a1b4d2b5126ed5b6586799cb22
Max Leibowitz <403089@bsd48.org>
2020-05-13 18:00:20 -0700

    Integrated mewtastic's bot (with permission) into rolebot

--- levelbot.py
+++ levelbot.py
@@ -1,21 +1,22 @@
-from typing import List
-
+# Discord bot (I use Arch btw) for Linux Galore.
+# Will manage some server moderation and pull Arch wiki pages on request.
 import discord
-import chatbot
-import json
 from discord.ext import commands
+import os
+import json
+
+os.chdir(r'/home/mewtastic/Code/Git/ArchBot')
 
 with open("secrets.json") as f:
     secrets = json.loads(f.read())
 
 client = commands.Bot(command_prefix='!')
 
-
+# Set acticity
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game('Searching for !help'))
-    print('We have logged in as {0.user}'.format(client))
-
+    print('ready')
 
 # Errors
 @client.event
@@ -140,55 +141,4 @@ async def level_up(users, user):
             await user.remove(remove)
 
 
-@client.event
-async def on_raw_reaction_add(reaction):
-    if not reaction.message_id == 709827022800683038:
-        guild = client.get_guild(reaction.guild_id)
-        readme: discord.TextChannel = guild.get_channel(709580982688153621)
-        message: discord.Message = await readme.fetch_message(709897964771999816)
-        check = message.reactions
-        for i in check:
-            if i.emoji == "✅":  # There IS a char there, but my IDE doesn't display it.  It's
-                if len(reaction.member.roles) < 2:
-                    await reaction.member.add_roles(discord.utils.get(guild.roles, id=709873263525757060))
-            else:
-                await message.remove_reaction(reaction.emoji, reaction.member)
-
-    role: str = reaction.emoji.name
-    roles = {"arch": 709562148342202368,
-             "gentoo": 709599034486292482,
-             "debian": 709572442858848256,
-             "fedora": 709572559280275548,
-             "manjaro": 709572766596202597,
-             "mint": 709572667560165458,
-             "pop": 709829747089211402,
-             "ubuntu": 709568934600507553,
-             "windoze": 709608463399125033,
-             "vim": 709572199488684134,
-             "emacs": 709572338009767977}
-    msg = await client.get_channel(709800728008327238).fetch_message(709827022800683038)
-    if role not in roles.keys():
-        await msg.remove_reaction(reaction.emoji, reaction.member)
-        return
-
-    guild = client.get_guild(reaction.guild_id)
-
-    readme: discord.TextChannel = guild.get_channel(709580982688153621)
-    message: discord.Message = await readme.fetch_message(709897964771999816)
-    check = message.reactions
-    check_box = None
-    for i in check:
-        if i.emoji == "✅":  # There IS a char there, but my IDE doesn't display it.  It's
-            check_box = i
-    if not check_box:
-        return
-    users = await check_box.users().flatten()
-    if reaction.member.name in list(map(lambda x: x.name, users)):
-        role = discord.utils.get(guild.roles, id=roles[role])
-        await reaction.member.add_roles(role)
-    else:
-        await msg.remove_reaction(reaction.emoji, reaction.member)
-        await reaction.member.send("Please react with the checkbox in #readme")
-
-
-client.run(secrets["rolebot"]["token"])
+client.run(secrets["archbot"]["token"])
