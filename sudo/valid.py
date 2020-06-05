93b51d88fb9e8371aa4bbf7be44329985c0b5921
Mewtastic <42871562+Mewtastic@users.noreply.github.com>
2020-06-04 22:47:00 -0700

    Document the code
    It's useless, but it's a good useless!

--- sudo/valid.py
+++ sudo/valid.py
@@ -1,11 +1,14 @@
+# A simple script to validate an int passed through the code to avoid errors.
 import re
 
 
 def validate_number(number):
-    if re.fullmatch("^[0|1|2|3|4|5|6|7|8|9]+$", number):
+    # matchall individual numbers to see if they are an int by themselves.
+    if re.fullmatch("^[0|1|2|3|4|5|6|7|8|9]+$", number): # Oddly checking an int with a str, but it works
+        # Make sure there is a value there in the first place and make sure the number isn't absurd.
         if 0 < len(number) < 5:
             return True
         else:
             return False
     else:
-        return False
+        return False # You need to have return false twice or else it won't return anything making error handling difficult
