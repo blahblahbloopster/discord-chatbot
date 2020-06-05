a29d8f468cb97ea29feb8e4ccdb14e6cb171e195
Mewtastic <42871562+Mewtastic@users.noreply.github.com>
2020-06-04 23:01:07 -0700

    Moar comments!
    UwU

--- sudo/image.py
+++ sudo/image.py
@@ -1,3 +1,4 @@
+# Script to edit the Mask.png for displaying a users level
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -9,7 +10,7 @@ if os.path.abspath(".").split("/")[-1] != "discord_bot":
 
 
 def draw(percentage, color=(255, 0, 0)):
-    """Creates a bar image"""
+    # Creates a bar image
     im2 = Image.open("sudo/mask.png")
     im3 = Image.new("RGBA", (1920, 240), color=(20, 20, 20))
     draw2 = ImageDraw.Draw(im3)
@@ -21,7 +22,7 @@ def draw(percentage, color=(255, 0, 0)):
 
 
 def make_image(username: discord.Member, level: int, xp: int, lvl_start_xp: int, lvl_end_xp: int):
-    """Creates an image that shows a user's level and XP"""
+    # Creates an image that shows a user's level and XP
     color = username.top_role.color.to_rgb()
     img = draw(((xp - lvl_start_xp) / (lvl_end_xp - lvl_start_xp)) * 100, color=color)
     draw2 = ImageDraw.Draw(img)
