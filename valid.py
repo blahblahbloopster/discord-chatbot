ca70b1cdc92b855dce3a223805453b8fcfcc209f
Mewtastic <42871562+Mewtastic@users.noreply.github.com>
2020-11-17 14:39:47 -0800

    Allow negatives

--- valid.py
+++ valid.py
@@ -2,7 +2,7 @@ import re
 
 
 def validate(number):
-    if re.fullmatch("^[0|1|2|3|4|5|6|7|8|9]+$", number):
+    if re.fullmatch("^[0|1|2|3|4|5|6|7|8|9|-]+$", number):
         if 0 < len(number) < 5:
             return True
         else:
